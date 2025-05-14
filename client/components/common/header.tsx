be81d10da9482a5d6f61b272f57b39be1ba98d1b
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2025-05-13 22:00:42 -0500

    Navigation

---
+++ client/components/common/header.tsx
@@ -0,0 +1,44 @@
+
+import React from "react";
+import { FileText } from "lucide-react";
+import { Button } from "../ui/button";
+import NavLink from "./nav-link";
+
+
+export default function Header() {
+    const isLoggedIn = false;
+  return (
+    <nav className="container flex justify-between items-center py-4 lg:px-8 mx-auto">
+      <div className="flex">
+        <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+          <FileText className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out " />
+          <span className="lg:text-lg font-extrabold text-gray-900">
+            Summarize
+          </span>
+        </NavLink>
+      </div>
+
+      <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
+        <NavLink href="#pricing">Pricing</NavLink>
+        <NavLink href="/dashboard">Your Summaries</NavLink>
+      </div>
+
+      <div className="flex lg:justify-end lg:flex-1">
+        {isLoggedIn ? (
+            <div className="flex gap-2 items-center">
+            <NavLink href="/upload">Upload a pdf</NavLink>
+            <div>Pro</div>
+            <Button>User</Button>
+        </div>
+        ):(
+            <div>
+            <NavLink href="/sign-in">Sign In</NavLink>
+            </div>
+        )}
+
+       
+
+      </div>
+    </nav>
+  );
+}
