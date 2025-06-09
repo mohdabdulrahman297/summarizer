9551d8e320d39cd1d8d05449d2e643cdaaa850eb
abdulrahmanmohd297 <abdulrahmanmohd297@gmail.com>
2025-06-09 15:47:07 -0500

    es-lint

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,4 +1,6 @@
-export const isDev = process.env.NODE_ENV === 'development';
-export const isProd = process.env.NODE_ENV === 'production';
+export const isDev = process.env.NODE_ENV === "development";
+export const isProd = process.env.NODE_ENV === "production";
 
-export const ORIGIN_URL = isDev ? 'http://localhost:3000' : '';
+export const ORIGIN_URL = isDev
+  ? "http://localhost:3000"
+  : process.env.NEXT_PUBLIC_ORIGIN_URL || "https://your-production-domain.com";
